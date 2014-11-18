9250c1da7615c31980805880cb08d84926c43b2a
Vadim Goncharov <vg@vadimgoncharov.ru>
2014-11-18 17:37:56 +0300

    fix bug with empty symbols in iTerm
    Add two-way convertion for fix bug with empty symbols in iTerm.

--- index.js
+++ index.js
@@ -1,87 +1,98 @@
 'use strict';
+
+var fromCharCode = String.fromCharCode;
 var platform = process.platform;
+var symbols;
 
-var main = {
-	tick: '✔︎',
-	cross: '✖',
-	star: '★',
-	square: '▇',
-	squareSmall: '◻',
-	squareSmallFilled: '◼',
-	circle: '◯',
-	circleFilled: '◉',
-	circleDotted: '◌',
-	circleDouble: '◎',
-	circleCircle: 'ⓞ',
-	circleCross: 'ⓧ',
-	circlePipe: 'Ⓘ',
-	circleQuestionMark: '?⃝',
-	bullet: '●',
-	dot: '․',
-	line: '─',
-	ellipsis: '…',
-	pointer: '❯',
-	info: 'ℹ',
-	warning: '⚠',
-	hamburger: '☰',
-	smiley: '㋡',
-	mustache: '෴',
-	heart: '♥',
-	arrowUp: '↑',
-	arrowDown: '↓',
-	arrowLeft: '←',
-	arrowRight: '→',
-	radioOn: '◉',
-	radioOff: '◯',
-	checkboxOn: '☒',
-	checkboxOff: '☐',
-	checkboxCircleOn: 'ⓧ',
-	checkboxCircleOff: 'Ⓘ',
-	questionMarkPrefix: '?⃝',
-};
+if (platform === 'win32') {
+	symbols = {
+		tick: '√',
+		cross: '×',
+		star: '*',
+		square: '█',
+		squareSmall: '[ ]',
+		squareSmallFilled: '[█]',
+		circle: '( )',
+		circleFilled: '(*)',
+		circleDotted: '( )',
+		circleDouble: '( )',
+		circleCircle: '(○)',
+		circleCross: '(×)',
+		circlePipe: '(│)',
+		circleQuestionMark: '(?)',
+		bullet: '*',
+		dot: '.',
+		line: '─',
+		ellipsis: '...',
+		pointer: '>',
+		info: 'i',
+		warning: '‼',
+		hamburger: '≡',
+		smiley: '☺',
+		mustache: '┌─┐',
+		heart: main.heart,
+		arrowUp: main.arrowUp,
+		arrowDown: main.arrowDown,
+		arrowLeft: main.arrowLeft,
+		arrowRight: main.arrowRight,
+		radioOn: '(*)',
+		radioOff: '( )',
+		checkboxOn: '[×]',
+		checkboxOff: '[ ]',
+		checkboxCircleOn: '(×)',
+		checkboxCircleOff: '( )',
+		questionMarkPrefix: '？'
+	};
+}
+else {
+	symbols = {
+		tick: '✔︎',
+		cross: '✖',
+		star: '★',
+		square: '▇',
+		squareSmall: '◻',
+		squareSmallFilled: '◼',
+		circle: '◯',
+		circleFilled: '◉',
+		circleDotted: '◌',
+		circleDouble: '◎',
+		circleCircle: 'ⓞ',
+		circleCross: 'ⓧ',
+		circlePipe: 'Ⓘ',
+		circleQuestionMark: '?⃝',
+		bullet: '●',
+		dot: '․',
+		line: '─',
+		ellipsis: '…',
+		pointer: '❯',
+		info: 'ℹ',
+		warning: '⚠',
+		hamburger: '☰',
+		smiley: '㋡',
+		mustache: '෴',
+		heart: '♥',
+		arrowUp: '↑',
+		arrowDown: '↓',
+		arrowLeft: '←',
+		arrowRight: '→',
+		radioOn: '◉',
+		radioOff: '◯',
+		checkboxOn: '☒',
+		checkboxOff: '☐',
+		checkboxCircleOn: 'ⓧ',
+		checkboxCircleOff: 'Ⓘ',
+		questionMarkPrefix: '?⃝',
+		};
 
-var win = {
-	tick: '√',
-	cross: '×',
-	star: '*',
-	square: '█',
-	squareSmall: '[ ]',
-	squareSmallFilled: '[█]',
-	circle: '( )',
-	circleFilled: '(*)',
-	circleDotted: '( )',
-	circleDouble: '( )',
-	circleCircle: '(○)',
-	circleCross: '(×)',
-	circlePipe: '(│)',
-	circleQuestionMark: '(?)',
-	bullet: '*',
-	dot: '.',
-	line: '─',
-	ellipsis: '...',
-	pointer: '>',
-	info: 'i',
-	warning: '‼',
-	hamburger: '≡',
-	smiley: '☺',
-	mustache: '┌─┐',
-	heart: main.heart,
-	arrowUp: main.arrowUp,
-	arrowDown: main.arrowDown,
-	arrowLeft: main.arrowLeft,
-	arrowRight: main.arrowRight,
-	radioOn: '(*)',
-	radioOff: '( )',
-	checkboxOn: '[×]',
-	checkboxOff: '[ ]',
-	checkboxCircleOn: '(×)',
-	checkboxCircleOff: '( )',
-	questionMarkPrefix: '？'
-};
-
-if (platform === 'linux') {
-  // the main one doesn't look that good on Ubuntu
-  main.questionMarkPrefix = '?';
+		if (platform === 'linux') {
+  			// the main one doesn't look that good on Ubuntu
+  			symbols.questionMarkPrefix = '?';
+		}
 }
 
-module.exports = platform === 'win32' ? win : main;
+// fix bug with empty symbols in iTerm
+Object.keys(symbols).forEach(function(key) {
+	symbols[key] = fromCharCode(symbols[key].charCodeAt(0));
+});
+
+module.exports = symbols;
